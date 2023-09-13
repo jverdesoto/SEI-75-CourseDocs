677602a512d6b493f75ba2a760d3cd36a6227bcc
ivanjorge <ivandcarvalho29@gmail.com>
2023-09-13 09:14:43 +0100

    week 2 wed

--- CLASSWORK/Unit_1/Week_2/DOM_Lab/js/scripts.js
+++ CLASSWORK/Unit_1/Week_2/DOM_Lab/js/scripts.js
@@ -2,142 +2,155 @@ const data = [
 	{
 		body: "Fugit itaque ipsam perferendis sequi harum.",
 		category: "Unknown generator",
-		cover: "https://picsum.photos/seed/2327/1920/1080",
+		cover: "https://picsum.photos/seed/2327/1920/400",
 		createdAt: "2015-05-18T20:48:08Z",
 		id: 1,
 		isDraft: false,
 		title: "dolorum",
 		views: 223,
-        authorAvatar: "https://i.pravatar.cc/200?img=1",
+        authorAvatar: "https://i.pravatar.cc/100?img=1",
         authorName: "Emily Smith"
 	},
 	{
-		category: "Eos culpa et voluptatem.",
+		body: "Eos culpa et voluptatem.",
 		category: "unknown generator",
-		cover: "https://picsum.photos/seed/0316/1920/1080",
+		cover: "https://picsum.photos/seed/0316/1920/400",
 		createdAt: "2018-08-15T19:55:08Z",
 		id: 2,
 		isDraft: false,
 		title: "perspiciatis rerum",
 		views: 403,
-        authorAvatar: "https://i.pravatar.cc/200?img=3",
+        authorAvatar: "https://i.pravatar.cc/100?img=3",
         authorName: "Benjamin Johnson"
 	},
 	{
 		body: "Illo saepe consequuntur a libero deserunt a eos, perferendis sint, accusantium qui dolorum sed at.",
 		category: "unknown generator",
-		cover: "https://picsum.photos/seed/4326/1920/1080",
+		cover: "https://picsum.photos/seed/4326/1920/400",
 		createdAt: "2016-06-12T00:16:07Z",
 		id: 3,
 		isDraft: false,
 		title: "molestiae odio nisi",
 		views: 994,
-        authorAvatar: "https://i.pravatar.cc/200?img=5",
+        authorAvatar: "https://i.pravatar.cc/100?img=5",
         authorName: "Emily Smith"
 	},
 	{
 		body: "Unde veniam.",
 		category: "unknown generator",
-		cover: "https://picsum.photos/seed/4729/1920/1080",
+		cover: "https://picsum.photos/seed/4729/1920/400",
 		createdAt: "2010-06-19T16:41:51Z",
 		id: 4,
 		isDraft: false,
 		title: "cumque est",
 		views: 178,
-        authorAvatar: "https://i.pravatar.cc/200?img=6",
+        authorAvatar: "https://i.pravatar.cc/100?img=6",
         authorName: "Benjamin Johnson"
 	},
 	{
 		body: "Debitis et neque facilis magnam.",
 		category: "unknown generator",
-		cover: "https://picsum.photos/seed/8392/1920/1080",
+		cover: "https://picsum.photos/seed/8392/1920/400",
 		createdAt: "2016-04-19T19:44:19Z",
 		id: 5,
 		isDraft: false,
 		title: "ad impedit sunt",
 		views: 733,
-        authorAvatar: "https://i.pravatar.cc/200?img=7",
+        authorAvatar: "https://i.pravatar.cc/100?img=7",
         authorName: "Emily Smith"
 	},
 	{
 		body: "Error nisi deserunt consectetur ea a.",
 		category: "unknown generator",
-		cover: "https://picsum.photos/seed/9839/1920/1080",
+		cover: "https://picsum.photos/seed/9839/1920/400",
 		createdAt: "2012-11-14T20:24:23Z",
 		id: 6,
 		isDraft: false,
 		title: "molestias",
 		views: 151,
-        authorAvatar: "https://i.pravatar.cc/200?img=8",
+        authorAvatar: "https://i.pravatar.cc/100?img=8",
         authorName: "Benjamin Johnson"
 	},
 	{
 		body: "Mollitia doloribus a reprehenderit vitae, incidunt incidunt.",
 		category: "unknown generator",
-		cover: "https://picsum.photos/seed/2230/1920/1080",
+		cover: "https://picsum.photos/seed/2230/1920/400",
 		createdAt: "2015-10-05T17:56:52Z",
 		id: 7,
 		isDraft: false,
 		title: "dolore",
 		views: 129,
-        authorAvatar: "https://i.pravatar.cc/200?img=39",
+        authorAvatar: "https://i.pravatar.cc/100?img=39",
         authorName: "Sarah Davis"
 	},
-	{
+	{ 
 		body: "Reprehenderit et, asperiores sed reprehenderit nisi, architecto a reprehenderit ipsa.",
 		category: "unknown generator",
-		cover: "https://picsum.photos/seed/9417/1920/1080",
+		cover: "https://picsum.photos/seed/9417/1920/400",
 		createdAt: "2012-01-16T01:37:42Z",
 		id: 8,
 		isDraft: false,
 		title: "labore quae nostrum",
 		views: 757,
-        authorAvatar: "https://i.pravatar.cc/200?img=48",
+        authorAvatar: "https://i.pravatar.cc/100?img=48",
         authorName: "Sarah Davis"
 	},
 	{
 		body: "Perspiciatis minima sit.",
 		category: "unknown generator",
-		cover: "https://picsum.photos/seed/8842/1920/1080",
+		cover: "https://picsum.photos/seed/8842/1920/400",
 		createdAt: "2013-06-23T14:40:58Z",
 		id: 9,
 		isDraft: false,
 		title: "inventore",
 		views: 448,
-        authorAvatar: "https://i.pravatar.cc/200?img=55",
+        authorAvatar: "https://i.pravatar.cc/100?img=55",
         authorName: "Emily Smith"
 	},
 	{
 		body: "Dolore perferendis deleniti ab, in molestiae quia, autem perspiciatis aliquam corrupti.",
 		category: "unknown generator",
-		cover: "https://picsum.photos/seed/5668/1920/1080",
+		cover: "https://picsum.photos/seed/5668/1920/400",
 		createdAt: "2017-09-25T10:48:38Z",
 		id: 10,
 		isDraft: false,
 		title: "a vel laborum non a",
 		views: 719,
-        authorAvatar: "https://i.pravatar.cc/200?img=69",
+        authorAvatar: "https://i.pravatar.cc/100?img=69",
         authorName: "Sarah Davis"
 	}
 ] 
 
-//create h2 for title
-/*const titlePost = document.createElement('h1')
-titlePost.innerHTML = data[0].title
-titlePost.classList.add('myClass')
-document.getElementById('app').appendChild(title)
-*/
-//console.log(data[0].title);
-const titlePost = document.createElement('h1')
-//to put content between the <> </>
-titlePost.innerHTML = data[0].cover
-//to add a class
-titlePost.classList.add('postZero')
-//getting the id I want and appending the title to it
-document.getElementById('app').appendChild(titlePost)
-//other way to do it
-//document.getElementById('app').innerHTML = `<h1>${data[0].title}</h1>`
 
 
-//blog container to render the posts
-/*const container = document.getElementById("app")*/
+const cardContainer = document.getElementById('card-container');
+
+for (let i = 0; i < data.length; i++) {
+  const card = document.createElement('div');
+  card.classList.add('card'); 
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.textContent = data[i].title;
+
+  const cardBody = document.createElement('p');
+  cardBody.textContent = data[i].body;
+
+  const cardCategory = document.createElement('h3');
+  cardCategory.classList.add('cat'); 
+  cardCategory.textContent = data[i].category;
+
+  const cardImage = document.createElement('img');
+  cardImage.src = data[i].cover;
+  cardImage.alt = data[i].title;
+
+  
+  card.appendChild(cardTitle);
+  card.appendChild(cardBody);
+  card.appendChild(cardCategory);
+  card.appendChild(cardImage);
+
+ 
+  cardContainer.appendChild(card);
+}
+
+
