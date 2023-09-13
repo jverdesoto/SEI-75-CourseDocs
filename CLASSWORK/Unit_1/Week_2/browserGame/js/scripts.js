02fe2fd0f01b1243263d5b9a1286bc06c6723f0c
weezleb <basil.housden@yahoo.com>
2023-09-13 12:27:16 +0100

    browser game update

--- CLASSWORK/Unit_1/Week_2/browserGame/js/scripts.js
+++ CLASSWORK/Unit_1/Week_2/browserGame/js/scripts.js
@@ -14,11 +14,15 @@ let winner; // null = no winner; 1 or -1 = winner; 'T' = Tied game
 
 // cached elements
 const messageEl = document.querySelector('h1');
+const playAgainBtn = document.querySelector('button');
+const markerEls = [...document.querySelectorAll('#markers > div')];
 
 // event listeners
+document.getElementById('markers').addEventListener('click', handleDrop);
+playAgainBtn.addEventListener('click', init);
 
 // functions
-
+init();
 // Initialise all state, then call render()
 function init() {
     // To visualise the board's mapping to the DOM,
@@ -44,9 +48,65 @@ function render() {
     renderControls();
 }
 
+// In repsonse to user interaction, update all impacted
+// then render
+function handleDrop(evt) {
+    const colIdx = markerEls.indexOf(evt.target);
+    // Guards
+    if (colIdx === -1) return;
+    // shortcut to the column array
+    const colArr = board[colIdx];
+    // find the index of the first 0 in colArr
+    const rowIdx = colArr.indexOf(0);
+    // update the board to state the current player value (turn)
+    colArr[rowIdx] = turn;
+    // switch player turn
+    turn *= -1;
+    winner = getWinner(colIdx, rowIdx);
+    render();
+}
+
+// check for winner in board
+// return null if no winner, 1/-2 if a player has won, 'T'
+function getWinner(colIdx, rowIdx) {
+    return checkVerticalWin(colIdx, rowIdx) ||
+    checkHorizontalWin(colIdx,rowIdx);
+}
+
+function checkHorizontalWin(colIdx, rowIdx) {
+    const adjCountLeft =countAdjacent(colIdx, rowIdx);
+    // countAdjacent(colIdx, rowIdx, 0, -1) === 3 ? board[colIdx][rowIdx] ; null;
+}
+
+// Vertical win
+function checkVerticalWin(colIdx, rowIdx) {
+    return countAdjacent(colIdx, rowIdx, 0, -1) === 3 ? board[colIdx][rowIdx] : null;
+}
+
+function countAdjacent(colIdx, rowIdx, colOffset, rowOffset) {
+    // shortcut bariable to the player value
+    const player = board[colIdx][rowIdx];
+    // Track count of adjacent cells with the same player value
+    let count = 0;
+    // initialise new coordinates
+    colIdx += colOffset;
+    rowIdx += rowOffset;
+    while (
+        // Ensure colIDX is within bounds of the board array
+        board[colIdx] !== undefined &&
+        board[colIdx][rowIdx] !== undefined &&
+        board[colIdx][rowIdx] === player
+        ) {
+        count++;
+        colIdx += colOffset;
+        rowIdx += rowOffset;
+    }
+    return count;
+}
+
 function renderBoard() {
     board.forEach(function (colArr, colIdx) {
-        colArr.forEach(function(cellVal, rowIdx){
+        colArr.forEach(function (cellVal, rowIdx) {
             const cellId = `c${colIdx}r${rowIdx}`;
             const cellEl = document.getElementById(cellId);
             cellEl.style.backgroundColor = COLOURS[cellVal];
@@ -66,5 +126,14 @@ function renderMessage() {
 }
 
 function renderControls() {
- // Ternary expression is the go to when you want 1 of 2 values returned
+    // Ternary expression is the go to when you want 1 of 2 values returned
+    // <cond expression> ? truthy exp> : <falsy exp>
+    playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
+    // Iterate over the marker elements to hide/show
+    // according to the column being full (no 0s) or not
+    markerEls.forEach(function (markerEl, colIdx) {
+        const hideMarker = !board[colIdx].includes(0) || winner;
+        markerEl.style.visibility = hideMarker ? 'hidden' : ' visible'
+    });
 }
+
