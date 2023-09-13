df5eb74d67e2c6364012af8cc7dd68f375ca814e
thstanton <thstanton@proton.me>
2023-09-13 12:26:25 +0100

    finished connect 4 game

--- CLASSWORK/Unit_1/Week_2/BrowserGame/js/scripts.js
+++ CLASSWORK/Unit_1/Week_2/BrowserGame/js/scripts.js
@@ -11,10 +11,14 @@ let turn // 1 or -1
 let winner // null = no winner (yet); 1 or -1 winner; 'T' = tie
 
 /*----- cached elements  -----*/
-
+const messageEl = document.querySelector('h1')
+const playAgainBtn = document.querySelector('button')
+// Creates an array from the node list using the spread operator, so that array methods can be used on it
+const markerEls = [...document.querySelectorAll('#markers > div')]
 
 /*----- event listeners -----*/
-
+document.getElementById("markers").addEventListener("click", handleDrop)
+playAgainBtn.addEventListener("click", init)
 
 /*----- functions -----*/
 init()
@@ -37,6 +41,83 @@ function init() {
     render()
 }
 
+// In response to user interaction, update all impacted state
+// Then call render()
+function handleDrop(evt) {
+    // Finds the index of the target of the event - eg. which div was clicked
+    const colIdx = markerEls.indexOf(evt.target)
+    // Guards...
+    if (colIdx === -1) return
+    //Shortcut to the column array
+    const colArr = board[colIdx]
+    // Find the index of the first 0 in colArr
+    const rowIdx = colArr.indexOf(0)
+    // Update the board state with the cur player value (turn)
+    colArr[rowIdx] = turn
+    // Switch player turn
+    turn *= -1
+    // Check for winner
+    winner = getWinner(colIdx, rowIdx)
+    // Render changes
+    render()
+}
+
+// Check for winner in board state - return null / 1 / -1 / 'T'
+function getWinner(colIdx, rowIdx) {
+    return checkVerticalWin(colIdx, rowIdx) ||
+        checkHorizontalWin(colIdx, rowIdx) ||
+        checkDiagonalWinNESW(colIdx, rowIdx) ||
+        checkDiagonalWinNWSE(colIdx, rowIdx)
+}
+
+// Functions which check different combinations of offsets for countAdjacent, 
+// returning the player or null according to the count
+function checkVerticalWin(colIdx, rowIdx) {
+    return countAdjacent(colIdx, rowIdx, 0, -1) === 3 ? board[colIdx][rowIdx] : null
+}
+
+function checkHorizontalWin(colIdx, rowIdx) {
+    const adjCountLeft = countAdjacent(colIdx, rowIdx, -1, 0)
+    const adjCountRight = countAdjacent(colIdx, rowIdx, 1, 0)
+    return (adjCountLeft + adjCountRight) >= 3 ? board[colIdx][rowIdx] : null
+}
+
+function checkDiagonalWinNESW(colIdx, rowIdx) {
+    const adjCountNE = countAdjacent(colIdx, rowIdx, 1, 1)
+    const adjCountSW = countAdjacent(colIdx, rowIdx, -1, -1)
+    return (adjCountNE + adjCountSW) >= 3 ? board[colIdx][rowIdx] : null
+}
+
+function checkDiagonalWinNWSE(colIdx, rowIdx) {
+    const adjCountNW = countAdjacent(colIdx, rowIdx, -1, 1)
+    const adjCountSE = countAdjacent(colIdx, rowIdx, 1, -1)
+    return (adjCountNW + adjCountSE) >= 3 ? board[colIdx][rowIdx] : null
+}
+
+// Counts numbers which match the current player
+function countAdjacent(colIdx, rowIdx, colOffset, rowOffset) {
+    // Shortcut variable to the player value
+    const player = board[colIdx][rowIdx]
+    // Track count of adjacent cells with the same player value
+    let count = 0
+    // Initialise new coordinates
+    colIdx += colOffset
+    rowIdx += rowOffset
+    while (
+        // Ensure colIdx is within bounds of the board array
+        board[colIdx] !== undefined &&
+        board[colIdx][rowIdx] !== undefined &&
+        // Continue to look for the current player's value
+        board[colIdx][rowIdx] === player
+    ) {
+        count ++
+        colIdx += colOffset
+        rowIdx += rowOffset
+    }
+
+    return count
+}
+
 // Visualise all state in the DOM
 function render() {
     renderBoard()
@@ -45,6 +126,7 @@ function render() {
     renderControls()
 }
 
+// Determine the background color of each cell, depending on their status in board
 function renderBoard() {
     board.forEach((colArr, colIdx) => {
         // Iterate over the cells in the current column (colArr)
@@ -56,10 +138,24 @@ function renderBoard() {
     })
 }
 
+// Show message - if T, if winner, if game in progress
 function renderMessage() {
-
+    if (winner === 'T') {
+        messageEl.innerText = "It's a Tie!!!"
+    } else if (winner) {
+        messageEl.innerHTML = `<span style="color: ${colours[winner]}">${colours[winner].toUpperCase()}</span> Wins!`
+    } else {
+        messageEl.innerHTML = `<span style="color: ${colours[turn]}">${colours[turn].toUpperCase()}</span>'s Turn`
+    }
 }
 
 function renderControls() {
-
+    // Hide or show the button depending on winner status (null / 1 / -1 / T)
+    playAgainBtn.style.visibility = winner ? 'visible' : 'hidden'
+    // Hide or show column headers when column is full (no 0s)
+    // Iterates through columnEls node list (array)
+    markerEls.forEach((markerEl, colIdx) => {
+        const hideMarker = !board[colIdx].includes(0) || winner
+        markerEl.style.visibility = hideMarker ? 'hidden' : 'visible'
+    })
 }
