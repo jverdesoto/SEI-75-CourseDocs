3619983d4957126c22a762db0cfdf1fa4a2c804f
hebaarafat159 <heba.arafat159@gmail.com>
2023-09-13 09:57:45 +0100

    upload BrowserGame and DOM_Events projects

---
+++ CLASSWORK/Unit_1/Week_2/BrowserGame/js/scripts.js
@@ -0,0 +1,26 @@
+console.log("Browser Game started");
+
+/*----- constants -----*/
+
+border , turn, winner
+
+/*----- state variables -----*/
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+function render(){}
+
+function init(){
+    bord = []//set values
+    trun = 1;
+winner = null;
+render();
+}
+
+init();
---
+++ CLASSWORK/Unit_1/Week_2/DOM_Events/js/scripts.js
@@ -0,0 +1,63 @@
+/**
+ * This function show a model that display error meesage,
+ * or the correct message that will be displayed for the user
+ * @param {*} message 
+ */
+function displayMessage(message){
+  
+   const myMessage = document.getElementById('messagTextId');
+   myMessage.innerText = message;
+
+   const modelView = new bootstrap.Model(document.getElementById('messageModelId'));
+   modelView.show();  
+}
+
+/**
+ * this function check if the string is empty or not and display error message.
+ * @param {*} key  // the input field label
+ * @param {*} value // the input field inserted value
+ * @returns 
+ */
+function isNotEmpty(key, value){
+    console.log(`${key} = ${value}`);
+    if(value.trim() !== undefined && value.trim() !== NaN && value.trim().length > 0)
+    {
+        console.log(`${key} is not empty`);
+        return true;
+    }else{
+        console.log(`${key} is empty, Please enter your ${key}`);
+        displayMessage(`Please enter your ${key}`);
+        return  false;
+    }
+}
+
+/**
+ * this function validate the inputs values and,
+ * display error message if the values are invalide
+ * @param {*} name // the inserted value in name input field
+ * @param {*} email // the inserted value in email input field
+ */
+function validateInput(name,email){
+    console.log(name);
+    if(isNotEmpty('name', name) && isNotEmpty('email', email))
+    {
+        console.log(`Hello ${name}, this is your email address: ${email}`);
+        displayMessage(`Hello ${name}, this is your email address: ${email}`);
+    }
+}
+
+/**
+ * this function will be called when sumbit button clicked,
+ * it reads name and email values from fields and send it for vaidation, 
+ * then display a user message. 
+ */
+function submitFunction(){
+    const name = document.getElementById('nameTextId').value;
+    const email = document.getElementById('emailTextId').value;
+    validateInput(name, email);
+} 
+
+document.getElementById('submitBtnId').addEventListener('click', (e) => {
+    e.preventDefault();
+    submitFunction();
+});
