c045767ae69ef46a13ba872a955190ea1f498912
msionut28 <moloceaionutt@gmail.com>
2023-09-13 14:25:18 +0300

    13/09/2023 - progress - Aditional JS

--- CLASSWORK/Unit_1/Week_2/browserGame/js/scripts.js
+++ CLASSWORK/Unit_1/Week_2/browserGame/js/scripts.js
@@ -11,10 +11,12 @@ let turn; // 1 or -1
 let winner; // null, 1 or -1 -> winner or T -> for a Tie
 
 /*----- cached elements  -----*/
-
+const messageEl = document.querySelector('h1');
+const playAgainBtn = document.querySelector('button');
+const markerEls = document.querySelectorAll('#markers > div')
 
 /*----- event listeners -----*/
-
+document.getElementById('markers').addEventListener('click', handleDrop)
 
 /*----- functions -----*/
 init();
@@ -58,9 +60,27 @@ function renderBoard() {
 }
 
 function renderMessage() {
-
+    if (winner === 'T') {
+        messageEl.innerText = "It's a Tie!!!";
+    }else if (winner) {
+        messageEl.innerHTML = `<span style="color: ${COLORS[winner]}">${COLORS[winner].toUpperCase()}</span>'s Turn`;
+    } else {
+        // Game is in play
+        messageEl.innerHTML = `<span style="color: ${COLORS[turn]}">${COLORS[turn].toUpperCase()}</span>'s Turn`;
+    }
 }
 
 function renderControls() {
+    // Ternary expression is the go to when you want 1 of 2 values returned
+    playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
+    // Iterate over the market elements to hide/show
+    // according to the column being full or not
+    markerEls.forEach(function(markerEl, colIdx) {
+        const hideMarker = !board[colIdx].includes(0) || winner;
+        markerEl.style.visibility = hideMarker ? 'hidden' : 'visible';
+    })
+}
+
+function handleDrop(evt) {
 
 }
