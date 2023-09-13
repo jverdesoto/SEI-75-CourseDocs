bd14d903f6aab94b41885f88cff80ee1c93973f0
MBurton-Johnson <matt.burton-johnson@pwc.com>
2023-09-13 12:30:37 +0100

    Browser Game update

--- CLASSWORK/Unit_1/Week_2/BrowserGame/js/script.js
+++ CLASSWORK/Unit_1/Week_2/BrowserGame/js/script.js
@@ -1,5 +1,9 @@
 // --------- constants   --------- //
-
+ const COLORS = {
+    '0': 'white',
+    '1': 'purple',
+    '-1': 'orange'
+ };
 
 // --------- state variables  --------- //
 let board; // array 7 columns arrays
@@ -8,10 +12,12 @@ let winner; // null = no winner; 1 or -1 = winner; 'T' = tie
 
 
 // --------- cached elements --------- //
-
+const messageEl = document.querySelector('h1');
+const playAgainBtn = document.querySelector('button');
+const markerEls = document.querySelectorAll('#markers > div')
 
 // --------- event listeners --------- //
-
+document.getElementById('markers').addEventListener('click', handleDrop);
 
 // --------- functions --------- //
 init();
@@ -23,12 +29,13 @@ function init() {
         // imagine the arrays below are rotated 90 degree counter-clockwise and you can visualise the board
 
     board = [ // don't use 'let board' as otherwise board would only be localised to this function. We want to call the global variable.
-        [0, 0, 0, 0, 0, -1], // col 0
+        [0, 0, 0, 0, 0, 0], // col 0
         [0, 0, 0, 0, 0, 0], // col 1
-        [-1, 0, 0, 0, 0, 0], // col 2
+        [0, 0, 0, 0, 0, 0], // col 2
         [0, 0, 0, 0, 0, 0], // col 3
         [0, 0, 0, 0, 0, 0], // col 4
-        [0, 0, 0, 0, 0, 1], // col 5
+        [0, 0, 0, 0, 0, 0], // col 5
+        [0, 0, 0, 0, 0, 0], // col 6
 ];
     turn = 1;
     winner = null;
@@ -49,16 +56,43 @@ function renderBoard() {
         colArr.forEach(function(cellVal, rowIdx) {
                 const cellId = `c${colIdx}r${rowIdx}`;
                 const cellEl = document.getElementById(cellId);
- 
-        console.log(colIdx, rowIdx, cellVal);
-    })
-    })
+                cellEl.style.backgroundColor = COLORS[cellVal];       
+    });
+    });
 }
 
 function renderMessage () {
+    if (winner === 'T') {
+        messageEl.innerText = "It's a Tie!!!";
+    } else if (winner) {
+        messageEl.innerHTML = `<span style="color: ${COLORS[winner]}">${COLORS[winner].toUpperCase()}</span> Wins!`
 
+    } else {
+
+        // Game is in play
+        messageEl.innerHTML = `<span style="color: ${COLORS[turn]}">${COLORS[turn].toUpperCase()}</span>'s Turn'`
+    }
 }
 
 function renderControls() {
+        // Ternary exp is go to when you want 1 of 2 values returned
+        //<conditional exp> ? <truthy exp> : <falsy exp>
 
+        //How I would code it ->
+
+        // if (winner != null) {
+        //     playAgainBtn.style.visibility = 'hidden'
+        // } else {
+        //     playAgainBtn.style.visibility = 'visible'
+        // }
+     
+        // How instructor would code it --->
+
+ playAgainBtn.style.visibility = winner ? 'visible': 'hidden';
+        //  Iterate over the marker elements to hide/show
+        // according to the column being full (no 0's) or not
+        markerEls.forEach((markerEl, colIdx) => {
+            const hideMarker = !board[colIdx].includes(0) || winner;
+            markerEl.style.visibility = hideMarker ? 'hidden' : 'visible';
+        });
 }
