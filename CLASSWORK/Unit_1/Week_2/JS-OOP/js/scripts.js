86108a2152a8ceda0a91cba5c830c5fddbbd00c2
Joe Freeman <joefreeman8@gmail.com>
2023-09-14 10:54:43 +0100

    COMMIT TO PULL

--- CLASSWORK/Unit_1/Week_2/JS-OOP/js/scripts.js
+++ CLASSWORK/Unit_1/Week_2/JS-OOP/js/scripts.js
@@ -3,7 +3,7 @@
     - OOP: Using OBJECTS to represent real world things.
     - Objects hace own properties and functions (methids)
     - Objects contain data and methods to represent info about what you're trying to model
-    ex: 
+    ex:
     function Ship() {
         this.floats = true
         this.material = "steel"
@@ -11,7 +11,7 @@
 
     - Data is encapsulated, conained within the scope
     - In classic OOP, a class is defined, then when an instance of is created, all the properties are *copied* over from the constructor to the instance
-    ex as a class: 
+    ex as a class:
     function Ship() {
         this.floats = true
         this.material = "steel"
@@ -26,7 +26,7 @@
     - Javascript inheritance is prototype based
     - Each Object has a private property called Prototype
     - The Prototype can have a Protoype of its own (walking up the chain)
-    ex: 
+    ex:
     const myObj {
         a: 1,
         b: 2
@@ -45,3 +45,70 @@
     console.log(mystring.vlaueOf())
     - Nearly of all objects in JS are instances of Object which seats on top of the Protitype chain
 */
+// *
+// function Ship() {
+//   this.floats = true
+//   this.material = "steel"
+//   this.whatAmI = function () {
+//     console.log(`I am made of ${this.material}`)
+//   }
+// }
+
+// const myShip = new Ship()
+// console.log(myShip.floats)
+
+// myShip.whatAmI()
+
+// const myString = "hello"
+
+// console.log(myString.__proto__)
+
+// * Class car
+// class Car {
+//   constructor(topSpeed, model, manufacturer, color) {
+//     this.topSpeed = topSpeed
+//     this.model = model
+//     this.manufacturer = manufacturer
+//     this.color = color
+//   }
+// }
+
+// const myCar = new Car(140, 'Focus', 'Ford', 'black')
+// const myCar2 = new Car(150, 'Golf', 'Volkswagen', 'silver')
+
+// console.log(`My ${myCar.manufacturer} ${myCar.model} is ${myCar.color} and has a top speed of ${myCar.topSpeed}mph`)
+
+// console.log(`My ${myCar2.manufacturer} ${myCar2.model} is ${myCar2.color} and has a top speed of ${myCar2.topSpeed}mph`)
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+
+  sayHello() {
+    console.log(`Hello, my name is ${this.firstName} ${this.lastName}.`)
+  }
+}
+
+const person = new Person('Joe', 'Freeman')
+person.sayHello()
+
+
+
+class Employee {
+  constructor(person, company, wage) {
+    this.person = person
+    this.company = company
+    this.wage = wage
+    this.active = true
+  }
+
+  receiveRaise(amount) {
+    this.wage += amount
+    console.log(`Hi ${this.person.firstName} ${this.person.lastName}, here is your new wage ${this.wage}`)
+  }
+}
+
+const employee = new Employee(person, 'apple', 250000)
+employee.receiveRaise(7500)
