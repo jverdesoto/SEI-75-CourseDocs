f35098bf0fd303a6e0987cb2acc5be8b4906397d
Ramsay <ramsayveitch123@gmail.com>
2023-09-17 11:33:52 +0100

    weekend changes

--- HOMEWORK/Unit_1/testing/_tests_/arrOfPrimeNumbers.test.js
+++ HOMEWORK/Unit_1/testing/_tests_/arrOfPrimeNumbers.test.js
@@ -1,5 +1,5 @@
 const generateFirstXPrimes = require('../js/arrOfPrimeNumbers.js')
 
 test('Test if a number is prime', () => {
-       expect(generateFirstXPrimes(2)).toBe(true)
+       expect(generateFirstXPrimes(5)).toEqual([2,3,5,7,11])
 })
--- HOMEWORK/Unit_1/testing/js/arrOfPrimeNumbers.js
+++ HOMEWORK/Unit_1/testing/js/arrOfPrimeNumbers.js
@@ -1,17 +1,32 @@
-function generateFirstXPrimes(X) {
+function primeTest (x) {
+console.log(x);
+if (x < 2) {
+return false
+}
+for(i = 2; i <=Math.sqrt(x); i++) {
+ if (x % i === 0){
+ return false;
+ }
+}
+return true;
+
+}
+
+
+function generateFirstXPrimes(x) {
     const primes = [];
     let num = 2;
   
-    while (primes.length < X) {
-      if (isPrime(num)) {
+    while (primes.length < x) {
+      if (primeTest(num)) {
         primes.push(num);
       }
       num++;
     }
-  
+  console.log(primes);
     return primes;
   }
 
 
 
- modules.export = generateFirstXPrimes;
+ module.exports = generateFirstXPrimes;
