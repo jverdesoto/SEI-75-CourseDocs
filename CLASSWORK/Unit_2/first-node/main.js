662e5b1b0484217a929f1f035f2e848add6f4eea
weezleb <basil.housden@yahoo.com>
2023-09-25 12:03:18 +0100

    node module

--- CLASSWORK/Unit_2/first-node/main.js
+++ CLASSWORK/Unit_2/first-node/main.js
@@ -6,3 +6,25 @@ fs.writeFile(
     function() {
   console.log('done creating file');
 });
+
+
+const daysOfWeek = require('./days-of-week')[1];
+console.log(daysOfWeek);
+
+const daysOfWeek = require('./days-of-week');
+
+const day = daysOfWeek.getWeekday(5);
+console.log(day);
+
+
+const random = require('./random');  // Import the function from random.js
+
+for (let i = 0; i < 10; i++) {
+  console.log(random(100, 200));  // Call the function and log the result
+}
+
+
+const circle = require('./circle');
+
+console.log(circle.area(50));  
+console.log(circle.circumference(75));  
